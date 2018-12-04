5f63279295e76c70c3146c8e240686dac2aa3007
Mike <beltre1996@yahoo.com>
2018-12-03 21:22:17 -0500

    Web Caesar

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ form = """
                 <input type="text" name="rot" value="0">
                 <p class="error"></p>
             </div>
-            <textarea type="text" name="text"></textarea>
+            <textarea type="text" name="text">{}</textarea>
             <br>
             <input type="submit">
         </form>
